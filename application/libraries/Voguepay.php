27d3c443dc1ffdd7b6352508b87ff61723c6728f
Engr Sam Chuks <sammychukwu4real@yahoo.com>
2016-06-12 18:47:54 +0100

    Create Voguepay.php
    Native class for processing voguepay payments

---
+++ application/libraries/Voguepay.php
@@ -0,0 +1,9 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Voguepay {
+
+                 public function some_method()
+                 {
+                 }
+}
