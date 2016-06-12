94585f20ad0e90176fe3dffeb420a317c0abc43e
Engr Sam Chuks <sammychukwu4real@yahoo.com>
2016-06-12 21:38:42 +0100

    Update Voguepay.php

--- application/libraries/Voguepay.php
+++ application/libraries/Voguepay.php
@@ -16,13 +16,25 @@ class Voguepay {
                 $this->CI =& get_instance();
         }
         
-         public function query_tranx($tranxId,$type)
-        {
-           //get the full transaction details as an json from voguepay
-	$json = file_get_contents('https://voguepay.com/?v_transaction_id='.$_POST['transaction_id'].'&type=json');
-	//create new array to store our transaction detail
-	$transaction = json_decode($json, true);
+                 public function query_tranx($tranxId,$type)
+                 { if($type=="json")
+					 { //get the full transaction details as an json from voguepay
+	                   $json = file_get_contents('https://voguepay.com/?v_transaction_id='.$tranxId.'&type=.'$type);
+	                   //create new array to store our transaction detail
+	                   $transaction = json_decode($json, true);
+					 }
+				   elseif($type=="xml")
+				     {
+					 }
+				   else
+				     { return $this-error_report(001,"Invalid Type requested");   }
 	
-                
-        }
-}
+	             }
+				 
+				 
+	             public function error_report($errCode="",$errString="")
+                  { }
+	
+	}
+                 }
+
