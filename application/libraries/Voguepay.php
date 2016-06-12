069d948e52f3e05d8116cc9b339348a91ecc8944
Engr Sam Chuks <sammychukwu4real@yahoo.com>
2016-06-12 18:52:20 +0100

    Update Voguepay.php

--- application/libraries/Voguepay.php
+++ application/libraries/Voguepay.php
@@ -2,7 +2,20 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Voguepay {
-
+        
+        protected $CI;
+        
+        
+        
+        
+        
+        
+        public function __construct($params)
+        {
+                // Assign the CodeIgniter super-object
+                $this->CI =& get_instance();
+        }
+        
                  public function some_method()
                  {
                  }
