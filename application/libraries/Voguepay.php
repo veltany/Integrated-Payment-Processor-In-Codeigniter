1097be7829cb543064692d9d7b85b915f30251f6
Engr Sam Chuks <sammychukwu4real@yahoo.com>
2016-06-12 21:22:01 +0100

    Update Voguepay.php

--- application/libraries/Voguepay.php
+++ application/libraries/Voguepay.php
@@ -16,7 +16,13 @@ class Voguepay {
                 $this->CI =& get_instance();
         }
         
-                 public function some_method()
-                 {
-                 }
+         public function query_tranx($tranxId,$type)
+        {
+           //get the full transaction details as an json from voguepay
+	$json = file_get_contents('https://voguepay.com/?v_transaction_id='.$_POST['transaction_id'].'&type=json');
+	//create new array to store our transaction detail
+	$transaction = json_decode($json, true);
+	
+                
+        }
 }
